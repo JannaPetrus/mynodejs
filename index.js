01343d50c4d65959a4902b0fb31c919a4765b8f7
jannapetrus <jannapetrus@gmail.com>
2017-06-08 14:58:32 +0300

    added express files

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-var Car = require('./car').carClass;
+//var Car = require('./car').carClass;
+//
+//var bmw = new Car("BMW");
+//bmw.logName();
 
-var bmw = new Car("BMW");
-bmw.logName();
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
